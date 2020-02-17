4839fdb928e0497a147dac942ae5a8ad46eb89b6
Mo Al Waili <mohammed.alwaili@gmail.com>
2020-02-17 13:28:04 +0100

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Mohammed Al Waili on 17/02/2020.
+//  Copyright © 2020 Mohammed Al Waili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
